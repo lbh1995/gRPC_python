ab472817084be36df4887f02bc9bda5ad9ef5a29
lbh1995_dell <1044072893@qq.com>
2020-11-11 15:00:37 +0800

    the init python grpc project

---
+++ server/greeter_server.py
@@ -0,0 +1,33 @@
+# ! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+import grpc
+from concurrent import futures
+from proto import helloworld_pb2, helloworld_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+
+    def SayHello(self, request, context):
+        print("服务端接收到用户请求："+request.name)
+        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
